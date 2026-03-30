f5df1ff200957b92b7a5f2263d4bc4d08458200c
nandohtorres <nandotorresfn@gmail.com>
2026-03-30 14:15:24 -0500

    chore: remove Sentry test button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,11 @@ import { ProgressContextProvider } from '@/context/ProgressContext'
 import MainPage from '@/pages/MainPage'
 import ErrorBoundary from '@/components/ErrorBoundary'
 
-// TODO: remove after confirming Sentry works
-function SentryTestButton() {
-  return (
-    <button onClick={() => { throw new Error('This is your first error!') }}>
-      Break the world
-    </button>
-  )
-}
-
 export default function App() {
   return (
     <ErrorBoundary>
       <ThemeContextProvider>
         <ProgressContextProvider>
-          <SentryTestButton />
           <MainPage />
         </ProgressContextProvider>
       </ThemeContextProvider>
